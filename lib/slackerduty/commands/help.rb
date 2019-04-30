dc24c87c4a02f386988e46eede2e2cd15896989a
Christian Gregg <c_arlt@hotmail.com>
2019-04-30 17:15:48 +0100

    Update help message

--- lib/slackerduty/commands/help.rb
+++ lib/slackerduty/commands/help.rb
@@ -20,13 +20,6 @@ module Slackerduty
             unsub POLICY_ID     - Unubscribe to an escalation policy
             subbed              - Show which policies you are subscribed to
             policies            - List escalation policies
-            incident NUMBER/ID  - Show a specific incident
-            incidents           - List currently triggered and acknowledged incidents
-                                  You may optionally pass params 't' or 'a' to scope the
-                                  returned list.
-                                  You may optionally pass an integer to increase or
-                                  decrease the number of incidents. (default: 10)
-                                  E.g.: /sd incidents a 15
 
             You can invoke slackerduty using /slackerduty or /sd
             ```
