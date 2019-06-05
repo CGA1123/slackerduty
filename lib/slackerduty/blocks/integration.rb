39bc8bb254859d3adeb5a20a02efed87aca8ad3d
Christian Gregg <c_arlt@hotmail.com>
2019-06-05 11:26:22 +0100

    Slackerduty::Integration -> Slackerduty::Blocks::Integration

--- lib/slackerduty/blocks/integration.rb
+++ lib/slackerduty/blocks/integration.rb
@@ -4,33 +4,35 @@ require_relative './integrations/bugsnag'
 require_relative './integrations/honeycomb'
 
 module Slackerduty
-  module Integration
-    INTEGRATIONS = {
-      'Bugsnag' => Integrations::Bugsnag,
-      'Honeycomb' => Integrations::Honeycomb
-    }.freeze
+  module Blocks
+    class Integration
+      INTEGRATIONS = {
+        'Bugsnag' => Integrations::Bugsnag,
+        'Honeycomb' => Integrations::Honeycomb
+      }.freeze
 
-    def initialize(incident, alerts)
-      @integration = find_integration(incident, alerts)
-    end
+      def initialize(incident, alerts)
+        @integration = find_integration(incident, alerts)
+      end
 
-    def present?
-      !@integration.nil?
-    end
+      def present?
+        !@integration.nil?
+      end
 
-    def as_json(*)
-      @integration.as_json
-    end
+      def as_json(*)
+        @integration.as_json
+      end
 
-    private
+      private
 
-    def find_integration(incident, alerts)
-      keys = INTEGRATIONS.keys
+      def find_integration(incident, alerts)
+        keys = INTEGRATIONS.keys
 
-      alert = alerts.find { |a| keys.include?(a.dig('integration', 'summary')) }
-      integration_klass = INTEGRATIONS[alert.dig('integration', 'summary')]
+        alert = alerts.find { |a| keys.include?(a.dig('integration', 'summary')) }
+        integration_klass = INTEGRATIONS[alert.dig('integration', 'summary')]
 
-      integration_klass&.new(incident, alert)
+        integration_klass&.new(incident, alert)
+      end
     end
   end
 end
