11f5e675af754f1c5c50eb1ef3e493875a6aef78
Christian Gregg <c_arlt@hotmail.com>
2019-06-05 11:32:06 +0100

    Add Slackerduty::Blocks::ForwardedBy#present?

--- lib/slackerduty/blocks/forwarded_by.rb
+++ lib/slackerduty/blocks/forwarded_by.rb
@@ -7,6 +7,10 @@ module Slackerduty
         @from = from
       end
 
+      def present?
+        !@from.nil?
+      end
+
       def to_slack
         @to_slack ||= Slack::BlockKit::Layout::Context.new do |context|
           context.mrkdwn(text: "This alert was forwarded to you by <@#{@from.slack_id}>")
