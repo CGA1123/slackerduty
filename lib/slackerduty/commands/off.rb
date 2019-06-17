313d2c79e1273c52900d2e9febb6311d227e5c7a
Christian Gregg <c_arlt@hotmail.com>
2019-06-17 22:11:15 +0100

    Fetch user via slack_id

--- lib/slackerduty/commands/off.rb
+++ lib/slackerduty/commands/off.rb
@@ -16,7 +16,7 @@ module Slackerduty
       end
 
       def call(user_id, _organisation, _args)
-        user = user_repository.find(user_id)
+        user = user_repository.from_slack_id(user_id)
 
         if user
           user_repository.update(user.id, notifications_enabled: false)
