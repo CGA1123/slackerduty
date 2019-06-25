0bd71e2fba8276db5c0a0919f31bb680cecf3537
Christian Gregg <c_arlt@hotmail.com>
2019-06-25 19:24:06 +0100

    Add channels, channel_subscriptions

---
+++ db/migrations/20190625181731_create_channels.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Hanami::Model.migration do
+  change do
+    create_table :channels do
+      foreign_key :organisation_id, :organisations
+      column :id, String, null: false, primary_key: true
+    end
+
+    create_table :channel_subscriptions do
+      foreign_key :channel_id, :channels, type: String
+      primary_key %i[channel_id escalation_policy_id]
+      column :escalation_policy_id, String, null: false
+    end
+  end
+end
