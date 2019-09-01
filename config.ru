22c35479b2c6dc323f6a019cbac9537dffe96de6
Christian Gregg <c_arlt@hotmail.com>
2019-09-02 00:10:42 +0100

    Revert "mount sidekiq in config.ru"
    This reverts commit f7e0b4b4b748b985728c11ac7d895efc70e696bf.

--- config.ru
+++ config.ru
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
 require './config/environment'
-require 'sidekiq/web'
-
-Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-  their_username = ::Digest::SHA256.hexdigest(username)
-  our_username = ::Digest::SHA256.hexdigest(Slackerduty::SIDEKIQ_USERNAME)
-
-  their_password = ::Digest::SHA256.hexdigest(password)
-  our_password = ::Digest::SHA256.hexdigest(Slackerduty::SIDEKIQ_PASSWORD)
-
-  Rack::Utils.secure_compare(their_username, our_username) &
-    Rack::Utils.secure_compare(their_password, our_password)
-end
-
-mount Sidekiq::Web, at: '/sidekiq'
 
 run Hanami.app
--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,25 @@ require 'bundler/setup'
 require 'hanami/model'
 require 'hanami/setup'
 require 'hanami/middleware/body_parser'
+require 'sidekiq/web'
 
 require_relative '../apps/web/application'
 require_relative '../apps/api/application'
 
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  their_username = ::Digest::SHA256.hexdigest(username)
+  our_username = ::Digest::SHA256.hexdigest(Slackerduty::SIDEKIQ_USERNAME)
+
+  their_password = ::Digest::SHA256.hexdigest(password)
+  our_password = ::Digest::SHA256.hexdigest(Slackerduty::SIDEKIQ_PASSWORD)
+
+  Rack::Utils.secure_compare(their_username, our_username) &
+    Rack::Utils.secure_compare(their_password, our_password)
+end
+
 Hanami.configure do
   mount Api::Application, at: '/api'
+  mount Sidekiq::Web, at: '/sidekiq'
   mount Web::Application, at: '/'
 
   middleware.use Hanami::Middleware::BodyParser, :json
