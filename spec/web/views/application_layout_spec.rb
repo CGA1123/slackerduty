6185f2dadade70af83956a71d29d568864c4590f
Christian Gregg <c_arlt@hotmail.com>
2020-05-25 17:58:56 +0100

    Fix cop

--- spec/web/views/application_layout_spec.rb
+++ spec/web/views/application_layout_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Web::Views::ApplicationLayout, type: :view do
   let(:user) { instance_double('user', email: 'email@email.co.uk') }
   let(:exposures) { Hash[current_user: user, params: {}, format: :html, flash: {}] }
-  let(:layout) { Web::Views::ApplicationLayout.new(exposures, 'contents') }
+  let(:layout) { described_class.new(exposures, 'contents') }
   let(:rendered) { layout.render }
 
   it 'contains application name' do
