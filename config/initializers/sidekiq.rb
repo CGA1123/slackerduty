1d785ccc5975897a06efe8f8377d47409c3a70cc
Christian Gregg <c_arlt@hotmail.com>
2020-06-09 23:09:22 +0100

    Honeykiq tracing? (#74)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,8 +7,16 @@ Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Honeykiq::ServerMiddleware
   end
+
+  config.client_middleware do |chain|
+    chain.add Honeykiq::ClientMiddleware
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: Slackerduty::REDIS_URL }
+
+  config.client_middleware do |chain|
+    chain.add Honeykiq::ClientMiddleware
+  end
 end
