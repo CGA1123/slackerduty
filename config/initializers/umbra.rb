0cac09f275b5ffacd122d2c156495483ef0a433c
Christian Gregg <c_arlt@hotmail.com>
2020-06-25 22:16:53 +0100

    REDISCLOUD_URL -> REDIS_URL

--- config/initializers/umbra.rb
+++ config/initializers/umbra.rb
@@ -3,7 +3,7 @@
 require 'umbra'
 
 Umbra.configure do |config|
-  config.redis_options = { url: ENV['REDISCLOUD_URL'] }
+  config.redis_options = { url: ENV['REDIS_URL'] }
   config.error_handler = proc { |e| puts "error: #{e}" }
   config.request_selector = proc { |env, _resp| env['REQUEST_METHOD'] == 'GET' }
 end
