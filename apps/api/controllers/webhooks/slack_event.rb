5292f97b8124e3b43cc77339c020864e59392a68
Christian Gregg <c_arlt@hotmail.com>
2020-07-01 22:43:12 +0100

    Add SlackEvent handler

---
+++ apps/api/controllers/webhooks/slack_event.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative './slack_verification'
+
+module Api
+  module Controllers
+    module Webhooks
+      class SlackEvent
+        include Api::Action
+        include SlackVerification
+
+        before :verify_slack_signature
+
+        params do
+          required(:challenge) { str? & filled? }
+        end
+
+        def call(params)
+          if params.valid?
+            self.status = 200
+            self.body = { challenge: params[:challenge] }
+          else
+            self.status = 422
+          end
+        end
+      end
+    end
+  end
+end
